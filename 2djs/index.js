442e40f8622d82d443aff2a63c8a346903ebcde9
Ryan Annis <tinnymint@gmail.com>
2017-02-20 15:57:02 -0800

    find neighbours

--- 2djs/index.js
+++ 2djs/index.js
@@ -6,6 +6,7 @@ const particles = [];
 /* Simulation Constants */
 const timestep = 1/30;
 const solverIterations = 10;
+const kernelSizeSquare = 0.1;
 
 /* Physical constraints and constants */
 const gravity = 9.81;
@@ -15,8 +16,22 @@ const initialiseParticles = () => {
 
 };
 
+/* Use naive O(n) method for now.
+   Returns a list of indcies for the neighbours of the particles at index. 
+*/
 const findNeighbours = (index) => {
+    const pos = particles[index].pos;
+    const results = [];
 
+    for(let i = 0; i < particles.length; i++){
+        /* Calculate eulerian distance */
+        if(index === i) continue;
+        const neighbourPos = particles[i].pos;
+        if((pos.x -neighbourPos.x) * (pos.x -neighbourPos.x) + (pos.y -neighbourPos.y) * (pos.y -neighbourPos.y) < kernelSizeSquare){
+            results.push(i);
+        }
+    }
+    return results;
 };
 
 const simulate = () => {
@@ -26,8 +41,9 @@ const simulate = () => {
         particles[i].xEst += timestep * particles[i].v;
     }
     
+
     for(let i = 0; i < particles.length; i++){
-        //Find Neighbours
+        particles[i].neighbours = findNeighbours(i);
     }
 
     for(let i = 0; i < solverIterations; i++){
