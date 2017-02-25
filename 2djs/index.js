83f55f3ab070a8a4f3c7463e89ed78ec450422cc
Ryan Annis <tinnymint@gmail.com>
2017-02-25 13:09:01 -0800

    wrapup

--- 2djs/index.js
+++ 2djs/index.js
@@ -1,27 +1,37 @@
 const simulateAll = document.getElementById('simulateAll');
 const stepper = document.getElementById('stepper');
+const timeDisplay = document.getElementById('timeDisplay');
 const canvas = document.getElementById('display');
 const ctx = canvas.getContext('2d');
 
 const particles = [];
+let grid = [];
+let gridOffsets = [];
+let horizontalCells = 0;
+
+let iteration = 0;
 
 /* Simulation Constants */
 const timestep = 1/60;
-const solverIterations = 10;
-const restDensity = 1000;
+const solverIterations = 3;
+const restDensity = 6000;
 const particleMass = 1;
 const kernelRadius = 0.1;
-const epsilon = 500;
+const epsilon = 300;
 
-const numParticles = 100;
+const numParticles = 400;
+const small = 0.00001;
 
 /* Physical constraints and constants */
-const gravity = vec2.fromValues(0, 9.8);
+const gravity = vec2.fromValues(0, 1);
 
+/* For now, just initializes particles in random locations in the top 1/4 of the screen
+ * to create a dam-break scenario */
 const initializeParticles = () => {
     for(let i = 0; i < numParticles; i++){
         particles.push({
-            pos: vec2.fromValues(Math.random(), Math.random()),
+            id: i,
+            pos: vec2.fromValues(Math.random(), Math.random() / 2 ),
             vel: vec2.fromValues(0, 0),
             newPos: vec2.fromValues(0, 0),
             deltaP: vec2.fromValues(0, 0),
@@ -29,6 +39,44 @@ const initializeParticles = () => {
             lambda: 0,
             density: 0,
         });
+    }
+}
+
+/* Uses counting sort to create a hashing grid for the particles */
+const createSpatialHashingGrid = () => {
+    horizontalCells = Math.ceil(1 / (kernelRadius));
+    
+    let gridSums = new Array((horizontalCells) * (horizontalCells)).fill(0);
+    grid = new Array(horizontalCells * horizontalCells).fill(0);
+    gridOffsets = new Array((horizontalCells) * (horizontalCells)).fill(0);
+    
+    for(let i = 0; i < numParticles; i++){
+        const x = Math.max(Math.min(Math.floor( particles[i].pos[0] / kernelRadius), horizontalCells - 1 ), 0);
+        const y = Math.max(Math.min(Math.floor( particles[i].pos[1] / kernelRadius), horizontalCells - 1 ), 0);
+        const bin = x + horizontalCells * y;
+
+        gridSums[bin]++;
+    }
+
+
+    for(let i = 1; i < gridSums.length; i++){
+        gridOffsets[i] = gridOffsets[i-1] + gridSums[i-1];
+    }
+    //console.log(gridOffsets);
+    //console.log(gridSums);
+    //console.log(gridOffsets);
+
+    let ct = 0;
+    for(let i = 0; i < numParticles; i++){
+        const x = Math.max(Math.min(Math.floor( particles[i].pos[0] / kernelRadius), horizontalCells - 1 ), 0);
+        const y = Math.max(Math.min(Math.floor( particles[i].pos[1] / kernelRadius), horizontalCells - 1 ), 0);
+        const bin = x + horizontalCells * y;
+
+        gridSums[bin]--;
+        console.assert(isFinite((gridSums[bin]) + gridOffsets[bin]), 'asdf');
+        
+        grid[ (gridSums[bin]) + gridOffsets[bin] ] = particles[i];
+        ct++;
     }
 }
 
@@ -57,6 +105,7 @@ const spiky = (p1, p2) => {
     return r;
 }
 
+/* Initializes the temporary positions of the particles.*/
 const predictPositions = () => {
     particles.forEach((p1) => {
         vec2.scaleAndAdd(p1.vel, p1.vel, gravity, timestep);
@@ -64,20 +113,81 @@ const predictPositions = () => {
     });
 }
 
+/* Uses the hash grid to crate a list of the neighbours for
+ * each particle */
 const updateNeighbours = () => {
     particles.forEach((p1) => {
         const neighbours = [];
-        particles.forEach((p2) => {
-            const r = vec2.create();
-            vec2.sub(r, p1.newPos, p2.newPos);
-            if( vec2.len(r) > 0 && vec2.len(r) < kernelRadius){
-                neighbours.push(p2);
+        const x = Math.max(Math.min(Math.floor( p1.pos[0] / kernelRadius), horizontalCells - 1 ), 0);
+        const y = Math.max(Math.min(Math.floor( p1.pos[1] / kernelRadius), horizontalCells - 1 ), 0);
+
+        const kernelRadius2 = kernelRadius * kernelRadius;
+
+        const masks = [[0, 0], /* Same grid */
+                      [1, 1], /* Top left */
+                      [0, 1], /* Top */
+                      [-1, 1], /* Top Right */
+                      [-1, 0], /* Right */
+                      [-1, -1], /* Bottom Right*/
+                      [0, -1], /* Bottom */
+                      [1, -1], /* Bottom Left */
+                      [1, 0]]; /* Left */
+        
+        //console.log('----');
+        for( let i = 0; i < masks.length; i++ ){
+            const mask = masks[i];
+            const newX = mask[0] + x;
+            const newY = mask[1] + y;
+
+            if( newX >= 0 && newY >= 0 && newX < horizontalCells && newY < horizontalCells){
+                const bin = newX + newY * horizontalCells;
+                const limit = bin < gridOffsets.length - 1? gridOffsets[bin + 1] : numParticles;
+                //console.log('gob', gridOffsets[bin]);
+                //console.log('lim', limit);
+                for( let q = gridOffsets[bin]; q < limit ; q++){
+                    const p2 = grid[q];
+                    const diff = vec2.create();
+                    vec2.sub(diff, p1.pos, p2.pos);
+                    const r2 = vec2.sqrLen( diff );
+                    if( r2 < kernelRadius2 ){
+                        neighbours.push(p2);
+                    }
+                }
+            }
+        }
+
+        /*const neighbours2 = [];
+
+        neighbours.sort((a, b) => a.id - b.id);
+
+        particles.forEach( p2 => {
+            const diff = vec2.create();
+            vec2.sub(diff, p1.pos, p2.pos);
+            const r2 = vec2.sqrLen( diff );
+            if( r2 < kernelRadius2 ){
+                neighbours2.push(p2);
             }
         });
+
+        let isDiff = false;
+        if(neighbours.length === neighbours2.length){
+            for(let i = 0; i < neighbours2.length; i++){
+                if(neighbours[i].id !== neighbours2[i].id){
+                    isDiff = true;
+                    break;
+                }
+            }
+        }
+        else{
+            isDiff = true;
+        }
+        console.assert(!isDiff, "error in grid spatial hashing");*/
+
         p1.neighbours = neighbours;
     });
 }
 
+/* Calculates the smoothed density (rho) at each particle's location */
 const calculateDensities = () => {
     particles.forEach((p1) => {
         let rhoSum = 0;
@@ -89,6 +199,8 @@ const calculateDensities = () => {
     });
 }
 
+/* Calculates lambda (the estimated distance along the constraint gradient)
+ * to reach rest density*/
 const calculateLambda = () => {
     particles.forEach((p1) => {
         const constraint = p1.density / restDensity - 1;
@@ -113,6 +225,7 @@ const calculateLambda = () => {
     });
 }
 
+/* Calculates the required distance to move the particle/ */
 const calculateDeltaP = () => {
     particles.forEach((p1) => {
         let lambdaSum = vec2.fromValues(0, 0);
@@ -126,15 +239,17 @@ const calculateDeltaP = () => {
     });
 };
 
+/* Updates the particle's position for rendering and uses verlet integration to update the velocity */
 const updatePosition = () => {
     particles.forEach((p1) => {
         vec2.scaleAndAdd(p1.vel, p1.newPos, p1.pos, -1);
         //console.log(p1.vel);
         vec2.scale(p1.vel, p1.vel, 1/timestep);
-        p1.pos = p1.newPos;
+        p1.pos = vec2.clone(p1.newPos);
     });
 };
 
+/* Extremely basic constraint handling (clamps to edge of box) */
 const constrainParticles = () => {
      particles.forEach((p1) => {
          //console.log(p1.pos);
@@ -166,6 +281,7 @@ const render = () => {
 
 const simulate = () => {
     predictPositions();
+    createSpatialHashingGrid();
     updateNeighbours();
     for(let i = 0; i < solverIterations; i++){
         calculateDensities();
@@ -174,14 +290,18 @@ const simulate = () => {
         constrainParticles();
     }
     updatePosition();
+    iteration++;
+    timeDisplay.innerHTML = (iteration /60).toFixed(2) + "s";
     //console.log(particles);
 }
 
 simulateAll.onclick = () => {
-    console.log('simulation step complete');
+    const t0 = performance.now();
     simulate();
+    const t1 = performance.now();
+    console.log(`Simulation step ${t1 - t0 }ms`);
     render();
-    window.setTimeout(() => simulateAll.onclick(), 500);
+    window.requestAnimationFrame(() => simulateAll.onclick());
 }
 
 stepper.onclick = () => {
--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-# DynamicFluidSimulation
+# SPH-FluidSolver-WebGL
 
-This is an implementation of "Position Based Fluids" by Muller et al.
+This is an implementation of "Position Based Fluids" by Muller et al. in JavaScipt.
+
+It uses work from:
+
+http://mmacklin.com/pbf_sig_preprint.pdf
+
+http://matthias-mueller-fischer.ch/publications/sca03.pdf
+
+http://wwwcg.in.tum.de/fileadmin/user_upload/Lehrstuehle/Lehrstuhl_XV/Research/Publications/2013/CreepingOctreeGrid/OctreeFluidSCA2014.pdf
+
+and http://www.cs.cornell.edu/courses/cs5643/2014sp/stuff/BridsonFluidsCourseNotes_SPH_pp83-86.pdf
+
 
 # Current Status
 
-Basic Features in 2D Demo has been completed but simulation is currently NOT STABLE due to oversimplfiied border conditions and lack of viscosity.
+The canvas based demo is currently working.  It can currently do 2000 particles on my machine at 10fps.
 
-3D GPU Algo will start once 2D demo is complete.
+Just run 2DDemo.html to view.
+
+This project has been abandoned because I want to implment a PIC/FLIP method instead, and some aspect inherent to SPH-based-methods make this extremely difficult without OpenCL/CUDA/Compute Shaders (paralell sort).
